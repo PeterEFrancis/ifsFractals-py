c842f500a10e4400c28d0f190add51ba3dea1a48
Peter E. Francis <franpe02@gettysburg.edu>
2021-04-11 15:09:20 -0400

    add example.html

--- ifsFractals/__init__.py
+++ ifsFractals/__init__.py
@@ -23,21 +23,21 @@ from IPython.display import IFrame
 
 
 ## Built-In Transformations
-def Scale(s):
+def Scale(s:float):
     return np.array([[s, 0, 0],[0, s, 0],[0, 0, 1]], dtype=np.float64)
-def Translate(a, b):
+def Translate(a:float, b:float):
     return np.array([[1, 0, a],[0, 1, b],[0, 0, 1]], dtype=np.float64)
-def Rotate(theta):
+def Rotate(theta:float):
     return np.array([[np.cos(theta), -np.sin(theta), 0],[np.sin(theta), np.cos(theta), 0],[0, 0, 1]], dtype=np.float64)
-def ShearX(t):
+def ShearX(t:float):
     return np.array([[1, t, 0],[0,1, 0],[0, 0, 1]], dtype=np.float64)
-def ShearY(t):
+def ShearY(t:float):
     return np.array([[1, 0, 0],[t,1, 0],[0, 0, 1]], dtype=np.float64)
-def ScaleX(s):
+def ScaleX(s:float):
     return np.array([[s, 0, 0],[0, 1, 0],[0, 0, 1]], dtype=np.float64)
-def ScaleY(s):
+def ScaleY(s:float):
     return np.array([[1, 0, 0],[0, s, 0],[0, 0, 1]], dtype=np.float64)
-def ScaleXY(s, t):
+def ScaleXY(s:float, t:float):
     return np.array([[s, 0, 0],[0,t, 0],[0, 0, 1]], dtype=np.float64)
 
 
