779314a5b1015818b60d406e41a3c424b5be11bf
Peter E. Francis <franpe02@gettysburg.edu>
2021-04-11 03:05:02 -0400

    1.0.1

--- ifsFractals/__init__.py
+++ ifsFractals/__init__.py
@@ -1,3 +1,4 @@
+
  #  _  __     ______              _        _
  # (_)/ _|   |  ____|            | |      | |
  #  _| |_ ___| |__ _ __ __ _  ___| |_ __ _| |___
@@ -6,43 +7,180 @@
  # |_|_| |___/_|  |_|  \__,_|\___|\__\__,_|_|___/
  #
 
+import numpy as np
+from numpy import *
+import matplotlib.pyplot as plt
+from matplotlib import collections as mc
+import random as rd
 
-# Load External packages
-import numpy as np                                      # for wicked fast arrays                                  MORE INFORMATION: https://docs.scipy.org/doc/
-from numpy import *                                     # for ease of math function use
-from numpy.linalg import *                              # for ease of linalg function use
-from numba import njit                                  # for compiling functions into C, so they run faster.     MORE INFORMATION: http://numba.pydata.org/
-import matplotlib.pyplot as plt                         # for simple plotting
-from matplotlib import collections as mc                # for simple plotting
-import random as rd                                     # for random numbers
-import time                                             # for timing
-from termcolor import colored                           # for colored print statements
-import os                                               # for deep file manipulation
-import imageio                                          # for gif making
-from matplotlib.ticker import FormatStrFormatter        # for FormatStrFormatter
-from typing import List                                 # for specification of types
-from PIL import Image                                   # for faster images
-import math                                             # for math
-from scipy.stats import linregress                      # for linear regressions
+from typing import List
+from PIL import Image
 
 
-# this should only be temporary
-from numba.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWarning, NumbaPerformanceWarning
-import warnings
-warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
-warnings.simplefilter('ignore', category=NumbaPendingDeprecationWarning)
-warnings.simplefilter('ignore', category=NumbaPerformanceWarning)
+from IPython.core.display import display, HTML
+from IPython.display import IFrame
 
 
 
+## Built-In Transformations
+def Scale(s):
+    return np.array([[s, 0, 0],[0, s, 0],[0, 0, 1]], dtype=np.float64)
+def Translate(a, b):
+    return np.array([[1, 0, a],[0, 1, b],[0, 0, 1]], dtype=np.float64)
+def Rotate(theta):
+    return np.array([[np.cos(theta), -np.sin(theta), 0],[np.sin(theta), np.cos(theta), 0],[0, 0, 1]], dtype=np.float64)
+def ShearX(t):
+    return np.array([[1, t, 0],[0,1, 0],[0, 0, 1]], dtype=np.float64)
+def ShearY(t):
+    return np.array([[1, 0, 0],[t,1, 0],[0, 0, 1]], dtype=np.float64)
+def ScaleX(s):
+    return np.array([[s, 0, 0],[0, 1, 0],[0, 0, 1]], dtype=np.float64)
+def ScaleY(s):
+    return np.array([[1, 0, 0],[0, s, 0],[0, 0, 1]], dtype=np.float64)
+def ScaleXY(s, t):
+    return np.array([[s, 0, 0],[0,t, 0],[0, 0, 1]], dtype=np.float64)
 
 
 
-# import supporting files
-from .figures import *
-from .generatingPoints import *
-from .wordFractals import *
-from .basic import *
+
+
+
+
+## Built-In Figures
+Box = np.array([ [0., 0., 1.], [1., 0., 1.], [1., 1., 1.], [0., 1., 1.], [0., 0., 1.], [1/8, 1/8, 1.], [1/8-1/16, 1/8+1/16, 1.] ]).T
+def rect(n):
+    return ScaleY(1/n) @ Box
+Line = np.array([ [0., 0., 1.], [1., 0., 1.] ]).T
+
+XBox = np.array([ [0., 0., 1.], [1., 0., 1.], [1., 1., 1.], [0., 1., 1.], [0., 0., 1.], [0.5, 0., 1.], [0.5, 1., 1.], [1., 1., 1.], [1., 0.5, 1.], [0., 0.5, 1.], [0., 0., 1.], [1/8, 1/8, 1.], [1/8-1/16, 1/8+1/16, 1.]]).T
+
+
+
+
+
+
+
+def mprint(mat, leftspace=1, fmt="g"):
+    # To display matrices. Slightly adapted from
+    # https://gist.github.com/braingineer/d801735dac07ff3ac4d746e1f218ab75.
+    col_maxes = [max([len(("{:"+fmt+"}").format(x)) for x in col]) for col in mat.T]
+    for x in mat:
+        print(''.ljust(leftspace), end="|  ")#("                         | ", end="")
+        for i, y in enumerate(x):
+            print(("{:"+str(col_maxes[i])+fmt+"}").format(y), end="  ")
+        print("|")
+
+
+
+
+
+
+
+def choose_random_index(weights):
+    r = rd.uniform(0, 1)
+    t = 0
+    s = weights[0]
+    while r > s:
+        t += 1
+        s = s + weights[t]
+    return min(t, len(weights) - 1)
+
+
+
+
+
+def opNorm(A):
+    G = array(A[:2,:2], dtype=float)
+    return sqrt(max(linalg.eig(G @ G.T)[0]))
+
+def check_transformations(transformations, verbose=False):
+    assert transformations is not None
+    failed = []
+    for i in np.arange(len(transformations)):
+        if opNorm(transformations[i]) >= 1:
+            failed = failed + [i+1]
+    if len(failed) == 0:
+        if verbose:
+            print('The opNorm of every transformation is less than 1 so all of the transformations are contraction mappings.')
+        else:
+            return True
+    elif len(failed) == 1:
+        if verbose:
+            print(f'The opNorm of transformation {failed[0]} is greater than or equal to 1 so is not a contraction mapping.')
+        else:
+            return False
+    elif len(failed) > 1:
+        if verbose:
+            print(f'The opNorm of transformations {failed} are greater than or equal to 1 so are not contraction mappings.')
+        else:
+            return False
+
+
+
+
+## figures
+
+def transform(figures, transformations):
+    new_figures = [np.array([[1,0],[0,1],[1,1]], dtype=np.float64)]
+    for M in figures:
+        for T in transformations:
+            new_figures = new_figures + [T @ M]
+    return new_figures[1:]
+
+def generate_figures(n, figures, transformations):
+    output_figures = transform(figures, transformations)
+    for i in np.arange(1,n):
+        output_figures = transform(output_figures, transformations)
+    return output_figures
+
+def plot_figures(figures:List[np.ndarray], size:int=4, width:float=1.5, color:str='blue'):
+    lines = [ [(1.1, 1.1), (1.1, 1.1)] ]
+    for M in figures:
+        lines = lines + [[ (M[0][i], M[1][i]), (M[0][i+1], M[1][i+1]) ] for i in np.arange(len(np.transpose(M))-1)]
+    lc = mc.LineCollection(lines[1:], linewidths=width, color=color)
+    fig, ax = plt.subplots(figsize=(size,size))
+    ax.add_collection(lc)
+    ax.set_aspect('equal')
+    ax.autoscale()
+    ax.plot()
+    return
+
+
+
+
+
+
+
+## points
+
+def generate_points(n, transformations, weights=None, head_start=100):
+    if weights is None:
+        weights = np.array([1] * len(transformations))
+    weights = weights / sum(weights)
+    start = np.array([0, 0, 1.0])
+    output = np.array([[start[0], start[1], 1.0]]*n)
+    for i in range(head_start): # do `head_start` iterations to start converging
+        output[0] = transformations[choose_random_index(weights)] @ output[0]
+    for i in range(1,n):
+        output[i] = transformations[choose_random_index(weights)] @ output[i-1]
+    return (output[:,0], output[:,1])
+
+def plot_points(points, size=6):
+    fig, ax = plt.subplots(figsize=(size, size))
+    ax.set_aspect('equal')
+    ax.plot(*points, '.', ms=0.75)
+
+
+
+
+
+def find_bounds(transformations, weights=None):
+    G = generate_points(1000, transformations=transformations, weights=weights)
+    xmin, xmax, ymin, ymax = np.min(G[0]), np.max(G[0]), np.min(G[1]), np.max(G[1])
+    errX, errY = (xmax-xmin)/10, (ymax-ymin)/10
+    return np.array([xmin-errX, xmax+errX, ymin-errY, ymax+errY], dtype=np.float64)
+
+
 
 
 
@@ -50,177 +188,88 @@ from .basic import *
 
 ## the Fractal class
 class Fractal(object):
-    def __init__(self, transformations, weights=np.array([0.]), size=10, color=(0,0,255)):
-        if transformations is None:
-            raise ValueError('ifsFractals: transformations cannot be NoneType.')
-        self.transformations = transformations
-        self.color = color
-        if all(weights == np.array([0.])):
-            self.weights = make_eq_weights(len(transformations))
-        else:
-            if len(weights) != len(transformations):
-                raise ValueError('ifsFractals: Weights do not match the transformations.')
-            if sum(weights) - 1 > .00001:
-                weights = weights / sum(weights)
-                # raise ValueError('ifsFractals: Weights do not sum to 1.')
-            self.weights = weights
+    def __init__(self, transformations, weights=None, size=10, color=(0,0,255)):
 
-        if size==0:
-            raise ValueError('ifsFractals: size cannot be 0.')
+        assert transformations is not None
+        self.transformations = transformations
+
+        self.color = color
+
+        self.set_weights(weights)
+
+        assert size != 0
         self.size = size
-        self.xmin, self.xmax, self.ymin, self.ymax = find_bounds(self.transformations,self.weights)
+
+        self.xmin, self.xmax, self.ymin, self.ymax = find_bounds(self.transformations, self.weights)
         self.bounds = (self.xmin, self.xmax, self.ymin, self.ymax)
                                #   V    this or should be an and, but then I have to figure out how to deal with "flat" fractals
-        if (self.xmax-self.xmin==0 or self.ymax-self.ymin==0) or not check_transformations(self.transformations, mode="quiet"):
-            raise ValueError('ifsFractals: Fractal converges to insignificance or absurdity.')
+        if (self.xmax-self.xmin==0 or self.ymax-self.ymin==0) or not check_transformations(self.transformations):
+            raise ValueError('Fractal converges to insignificance or absurdity.')
         self.width = math.ceil((self.xmax-self.xmin)*36*self.size)
         self.height = math.ceil((self.ymax-self.ymin)*36*self.size)
-        self.isZoomed = False
 
         self.point = np.array([0,0,1])
         self.developement = 0
         self.pic = Image.new('RGB', (self.width, self.height), (255, 255, 255))
         self.pixels = self.pic.load()
 
+    def set_weights(self, weights=None):
+        if weights is None:
+            weights = np.array([1] * len(self.transformations))
+        assert len(weights) == len(self.transformations)
+        assert all([i >= 0 for i in weights])
+        assert any([i > 0 for i in weights])
+        self.weights = weights / sum(weights)
+
     def _scale(self, point):
         h = self.width * (point[0]-self.xmin)/(self.xmax-self.xmin)                 # (x + 2.182)*(self.width - 1)/4.8378                         Why take an input?
         k = self.height * (self.ymax-point[1])/(self.ymax-self.ymin)                # (9.9983 - y)*(self.height - 1)/9.9983
         return h, k
 
-    def _iterate(self):
-        r = rd.uniform(0, 1)
-        t = 0
-        sum = self.weights[0]
-        while r > sum:
-            t += 1
-            sum += self.weights[t]
-        self.point = self.transformations[min(t, len(self.weights)-1)] @ self.point
+    def plot_figures(self, depth=1, initial=Box, size:int=4, width:float=1.5, color:str='blue'):
+        figures = generate_figures(depth, [initial], self.transformations)
+        plot_figures(figures, size=size, width=width, color=color)
+
+    def check_transformations(self, verbose=False):
+        return check_transformations(self.transformations, verbose)
+
+    def iterate(self):
+        self.point = self.transformations[choose_random_index(self.weights)] @ self.point
+        return
 
     def add_points(self, n):
-        for _ in range(100):
-            self._iterate()
         for _ in range(n):
-            self._iterate()
+            self.iterate()
             self.pixels[self._scale(self.point)] = self.color
         self.developement += n
 
-    def set_zoom(self, isZoomed=True, xmin=None, xmax=None, ymin=None, ymax=None):
-        self.isZoomed = isZoomed
-        if xmin is not None:
-            self.xmin = xmin
-        if xmax is not None:
-            self.xmax = xmax
-        if ymin is not None:
-            self.ymin = ymin
-        if ymax is not None:
-            self.ymax = ymax
-
-    def add_points_zoom(self, n, x, y):
-        self.point = np.array([x,y,1])
-        for _ in range(100):
-            self._iterate()
-            while (self.xmin>self.point[0]) or (self.xmax<self.point[0]) or (self.ymin>self.point[1]) or (self.ymax<self.point[1]):
-                self._iterate()
-        for _ in range(n):
-            self._iterate()
-            while (self.xmin>self.point[0]) or (self.xmax<self.point[0]) or (self.ymin>self.point[1]) or (self.ymax<self.point[1]):
-                self._iterate()
-            self.pixels[self._scale(self.point)] = self.color
-
-    def make_gif(self, name='GIF', n=100_000, zoom=2, frames=7, zoomPoint=None):
-        if not os.path.exists('Saved Fractals/For Zooms'):
-            os.makedirs('Saved Fractals/For Zooms')
-        start = time.time()
-        print(f'Generating {frames} Zoomed images: ', end='')
-        if zoomPoint is None:
-            x, y = (self.xmax + self.xmin)/2, (self.ymax + self.ymin)/2
-        else:
-            x, y = zoomPoint
-        for frame in range(frames+1):
-            step = Fractal(self.transformations, self.weights, self.size, self.color)
-            scale = 1 - ((zoom-1)/zoom)*sqrt(frame/frames)
-            print(f'{frame}({round(1/scale, 2)})  ', end='')
-            xminZ = x - (self.xmax - self.xmin) * scale / 2
-            xmaxZ = x + (self.xmax - self.xmin) * scale / 2
-            yminZ = y - (self.ymax - self.ymin) * scale / 2
-            ymaxZ = y + (self.ymax - self.ymin) * scale / 2
-            step.set_zoom(True, xminZ, xmaxZ, yminZ, ymaxZ)
-            step.add_points_zoom(n, x, y)
-            step.save_pic(f'Saved Fractals/For Zooms/{name}_{frame}.png')
-        images = [imageio.imread(f'Saved Fractals/For Zooms/{name}_0.png')]*frames
-        for frame in range(frames+1):
-            images.append(imageio.imread(f'Saved Fractals/For Zooms/{name}_{frame}.png'))
-            os.remove(f'Saved Fractals/For Zooms/{name}_{frame}.png')
-        imageio.mimsave(f'Saved Fractals/{name}.gif', images)
-        end = time.time()-start
-        print(f'\nCompleted in {int(end//60)} minutes and {round(end%60)} seconds.')
-
-    def load_in_points(self, externalTup, frame=None):
+    def load_in_points(self, externalTup):
         n = len(externalTup[0])
         externalArray = np.array([*externalTup,[1.]*n]).T
-        if frame is None:
-            for row in externalArray:
-                self.pixels[self._scale(row)] = self.color
-            self.developement += n
-        else:
-            for row in externalArray:
-                if (frame[0]<=row[0]) and (frame[1]>=row[0]) and (frame[2]<=row[1]) and (frame[3]>=row[1]):
-                    self.pixels[self._scale(row)] = self.color
-            self.developement += n
+        for row in externalArray:
+            self.pixels[self._scale(row)] = self.color
+        self.developement += n
 
-    def save_pic(self, path):
+    def save(self, path):
         self.pic.save(path)
 
-    def display_pic(self):
+    def display(self):
         # https://stackoverflow.com/a/26649884
         plt.imshow(np.asarray(self.pic))
 
-    def dark_pixels(self):
-        return np.count_nonzero(255 - np.asarray(self.pic)) // 3
+    def export(self):
+        for T in self.transformations:
+            print(T.tolist())
 
-    def dimension(self, n=3_000_000, startSize=2, endSize=100, samples=None):
-        start = time.time()
-        test = Fractal(self.transformations, weights=self.weights, size = endSize, color=(0,0,0))
-        G = _generate_points_full(n, self.transformations, weights=self.weights)
-        test.load_in_points(G)
-        tag = str(time.time())
-        tag = tag[:10] + '_' + tag[11:]
-        test.pic.save(f'DIMENSION_CALCULATION_MAX_SIZE_CHECK_{tag}.png')
-        print(f'Verify that the image of the fractal with {n} points is \'filled in\' at size {endSize}. (Image file is in parent directory).')
-        elapsed = time.time() - start
-        go = input("Press Enter to continue or type stop.")
-        middle = time.time()
-        if (go == 'stop') or (go == 'STOP') or (go == 'Stop'):
-            print('Dimension calculation terminated.')
-            os.remove(f'DIMENSION_CALCULATION_MAX_SIZE_CHECK_{tag}.png')
-            return None
-        os.remove(f'DIMENSION_CALCULATION_MAX_SIZE_CHECK_{tag}.png')
-        print('Computing fractal dimension ', end='')
-        # find samples
-        if samples is None:
-            samples = endSize - startSize
-            for j in range(endSize - startSize):
-                samples -= j
-                potentialArray = np.floor(np.geomspace(startSize, endSize, num=samples))
-                if potentialArray[0] != potentialArray[1]:
-                    break
-        print(f'with {samples} samples...')
-        numberOfPixelsDark = np.array([0.]*samples)
-        numberOfPixelsDark[samples - 1] = test.dark_pixels()
-        # https://docs.scipy.org/doc/numpy/reference/generated/numpy.geomspace.html#numpy.geomspace
-        scalingFactor = np.floor(np.geomspace(startSize, endSize, num=samples))
-        for i in range(samples-1):
-            if scalingFactor[i] == scalingFactor[i-1]:
-                numberOfPixelsDark[i] = numberOfPixelsDark[i-1]
-                continue
-            test = Fractal(self.transformations, weights=self.weights, size=scalingFactor[i], colour=(0,0,0))
-            test.load_in_points(G)
-            numberOfPixelsDark[i] = test.dark_pixels()
-            if i == 0:
-                print(f'Finished samples with sizes: {int(np.floor(startSize))}', end='')
-            elif i == samples - 2:
-                print(f', {int(scalingFactor[i])}, {int(scalingFactor[i+1])}.')
-            else:
-                print(f', {int(scalingFactor[i])}', end='')
-        print(f'Completed in {np.round((elapsed + time.time() - middle)/60, decimals = 1)} minutes.')
-        return np.round(linregress(np.log(scalingFactor), np.log(numberOfPixelsDark))[0], decimals = 2)
+    def link(self):
+        matrices = [(np.round(T * 1000) / 1000).tolist() for T in self.transformations]
+        link = 'https://ifs-fractals.herokuapp.com/playground/t='
+        for m in matrices:
+            link += f'M({m[0][0]},{m[0][1]},{m[1][0]},{m[1][1]},{m[0][2]},{m[1][2]})&'
+        return link[:-1] + "/w=" + ",".join([str(el) for el in self.weights])
+
+    def link_web(self):
+        display(HTML("<a href='" + self.link() + "' target='_blank '>Click me to open in IFS Fractals</a>"))
+
+    def embed_web(self):
+        display(IFrame(self.link(), width='100%', height='1000px'))
--- setup.py
+++ setup.py
@@ -8,24 +8,20 @@ with open("README.md", "r") as fh:
 setup(
     name="ifsFractals",
     packages=["ifsFractals"],
-    version="0.1.6",
+    version="1.0.1",
     author="Peter Francis",
     author_email="franpe02@gettysburg.edu",
     description="For Generating Fast IFS Fractals",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/francisp336/ifsFractals",
+    url="https://github.com/PeterEFrancis/ifsFractals",
     classifiers=[],
     python_requires='>=3.0.0',
     install_requires=[
         'imageio',
         'matplotlib',
-        'numba==0.45.1',
         'numpy',
-        'Pillow',
-        'scipy',
-        # 'system',
-        'typing'
+        'Pillow'
     ],
     keywords='Fractal Generator'
 )
